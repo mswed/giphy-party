72070b8fdc0016603760681562c7e4ecf8825f0c
mswed <mswed@beapot.com>
2023-10-15 20:23:51 -0400

    basic search works

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Let's get this party started!");
+
+const api_key = 'bHvU9ATgW0XdgCT2v8k0wJ6GD1c2pQG9'
+async function searchGiphy(q){
+    console.log(q)
+    const res = await axios.get('https://api.giphy.com/v1/gifs/search', {params: {api_key, q}})
+    console.log(res)
+}
